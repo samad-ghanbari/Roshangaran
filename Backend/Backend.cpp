cf20a8143cded131eb556a0d8eaf2f70a0ef6dae
Samad Ghanbari <ghanbari.samad@gmail.com>
2024-10-13 12:16:35 +0330

    purify backend

--- Backend/Backend.cpp
+++ Backend/Backend.cpp
@@ -1,20 +1,13 @@
 #include "Backend.h"
 #include "Lib/database/dbman.h"
 #include <QJsonObject>
-//models
-//#include "Lib/models/BranchModel.h"
 
 Backend::Backend(QGuiApplication &app, QObject *parent)
     : QObject{parent}, dbMan(nullptr)
 {
     dbMan = new DbMan(this);
-    //branchModel = new BranchModel(dbMan, this);
 
     QQmlApplicationEngine engine;
-
-    //register
-    // i need to pass dbman so i use contextProperty instead of class registratin
-    //qmlRegisterType<BranchModel>("BranchModel",1,0,"BranchModel");
 
     QObject::connect(
         &engine,
@@ -33,15 +26,7 @@ void Backend::initiate()
 
 void Backend::loadHome()
 {
-
-    //qmlRegisterType<BranchModel>("Lib.models.BranchModel",1,0,"BranchModel");
-    //qmlRegisterUncreatableType<BranchModel>("models.BranchModel", 1,0, "BranchModel", "");
-    //BranchModel *branchModel = new BranchModel(this);
-
     QJsonObject user = dbMan->getUserJson();
     engine.rootContext()->setContextProperty("user", user);
-    //models
-    //-engine.rootContext()->setContextProperty("BranchModel", branchModel);
-
     engine.loadFromModule("Roshangaran", "HomeWindow");
 }
